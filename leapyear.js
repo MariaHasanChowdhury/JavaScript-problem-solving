974d7bee778e479fc85ce53937c517772d68503a
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-02-11 23:59:50 +0600

    call function

--- leapyear.js
+++ leapyear.js
@@ -9,3 +9,8 @@ function isLeapYear(year) {
         return false;
     }
 }
+const isYear1 = isLeapYear(2100);
+const isYear2 = isLeapYear(2400);
+const isYear3 = isLeapYear(1900);
+const isYear4 = isLeapYear(2052);
+console.log(isYear1, isYear2, isYear3, isYear4);
