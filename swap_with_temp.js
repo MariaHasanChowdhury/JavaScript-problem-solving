ae30905cf4c4ca82a561669cbad541ca305e5e22
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-02-12 18:52:06 +0600

    swap with temp

---
+++ swap_with_temp.js
@@ -0,0 +1,12 @@
+let num1 = 5;
+let num2 = 7;
+
+console.log('Before swaping:');
+console.log('num1 = ' + num1 + ' num2 = ' + num2);
+
+const temp = num1;
+num1 = num2;
+num2 = temp;
+console.log('After swaping:');
+console.log('num1 = ', num1);
+console.log('num2 = ', num2);
